9554fe866b96bbe761980f014ef8b8618bdad456
Max <max94220@hotmail.fr>
2024-04-09 16:35:08 +0200

    act_11&12

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Layout from "./components/Layout.jsx";
+import Notfound from "./pages/Notfound.jsx";
+import DishDetails from "./pages/DishDetails.jsx";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <Routes>
           <Route element={<Layout/>}>
             <Route index element={<Home/>}/>
+            <Route path="/plat/:id" element={<DishDetails />} />
+            <Route path='*' element={<Notfound/>}/>
           </Route>
         </Routes>
     </>
---
+++ src/pages/Notfound.jsx
@@ -0,0 +1,17 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const Notfound = () => {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <h1>Page non trouvée</h1>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Notfound;
