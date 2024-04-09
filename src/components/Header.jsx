2c96bb59719c508939289b44c9aaa631fb50b8a7
Max <max94220@hotmail.fr>
2024-04-09 11:55:33 +0200

    act_8

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,36 @@
-function Header(){
-    return(
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "../assets/styles/header.css";
+import logo from "../assets/images/logo.webp"
+
+const Header = () => {
+    return (
         <header>
-            <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="mexicain qui fait de la guitare"/>
-            <nav>
-                <ul>
-                    <li><a href="/">Acceuil</a></li>
-                </ul>
-            </nav>
+            <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">
+                <img
+                    src={logo}
+                    alt="Un mexicain qui joue de la guitare"
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse
+                id="basic-navbar-nav"
+                className="justify-content-end"
+                >
+                <Nav>
+                    <Nav.Link href="/">Accueil</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
         </header>
-        
-    )
-}
+    );
+};
+
 export default Header;
