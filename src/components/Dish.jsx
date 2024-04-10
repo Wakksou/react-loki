4492febacc3d63d9064ad6cee3bd46337a2ee1c6
Max <max94220@hotmail.fr>
2024-04-10 10:08:09 +0200

    act_13v1

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,16 +1,19 @@
 import Card from "react-bootstrap/Card";
 import "../assets/styles/dish.css";
+import { Link } from 'react-router-dom';
 
 const Dish = ({ name, img, price }) => {
-  return (
-    <Card>
-      <Card.Img variant="top" src={img} alt={name} />
-      <Card.Body>
-        <Card.Title>{name}</Card.Title>
-        <Card.Text>{price}€</Card.Text>
-      </Card.Body>
-    </Card>
-  );
+        return (
+                <Link to={'plat/${slug}'}>
+                        <Card>
+                                <Card.Img variant="top" src={img} alt={name} />
+                                <Card.Body>
+                                        <Card.Title>{name}</Card.Title>
+                                        <Card.Text>{price}€</Card.Text>
+                                </Card.Body>
+                        </Card>
+                </Link>
+        );
 };
 
 export default Dish;
---
+++ src/pages/About.jsx
@@ -0,0 +1,32 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { useParams } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+
+const About = () => {
+    const { slug } = useParams();
+    return (
+        <Container>
+            <Row>
+                <Col>
+                <Card>
+                    <h1>A propos </h1>
+                    <Card.Body>Bienvenue dans notre restaurant mexicain, où l'authenticité et la saveur se rencontrent pour créer une
+                    expérience culinaire inoubliable. Plongez dans un voyage gustatif à travers le Mexique avec notre menu
+                    varié, mettant en vedette des plats traditionnels préparés avec amour et expertise.
+                    Chaque bouchée est une explosion de saveurs, que ce soit avec nos tacos à l’unité, des enchiladas
+                    généreusement garnies ou notre mole poblano exquis. Nous croyons en l'utilisation d'ingrédients frais
+                    et de qualité pour créer des plats qui vous transportent directement au cœur de la cuisine mexicaine.
+                    Que vous soyez un amateur de plats épicés ou que vous préfériez des saveurs plus douces, notre menu
+                    offre quelque chose pour satisfaire toutes les papilles gustatives. Venez nous rejoindre pour une
+                    expérience culinaire authentique et chaleureuse dans une ambiance conviviale et accueillante. ¡Buen
+                    provecho!</Card.Body>
+                </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default About;
