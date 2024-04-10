9b67890048a5a3159be866aa830e2b84180b58c2
Max <max94220@hotmail.fr>
2024-04-10 11:44:01 +0200

    act_15

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,16 +12,25 @@ const About = () => {
                 <Col>
                 <Card>
                     <h1>A propos </h1>
-                    <Card.Body>Bienvenue dans notre restaurant mexicain, où l'authenticité et la saveur se rencontrent pour créer une
-                    expérience culinaire inoubliable. Plongez dans un voyage gustatif à travers le Mexique avec notre menu
-                    varié, mettant en vedette des plats traditionnels préparés avec amour et expertise.
-                    Chaque bouchée est une explosion de saveurs, que ce soit avec nos tacos à l’unité, des enchiladas
-                    généreusement garnies ou notre mole poblano exquis. Nous croyons en l'utilisation d'ingrédients frais
-                    et de qualité pour créer des plats qui vous transportent directement au cœur de la cuisine mexicaine.
-                    Que vous soyez un amateur de plats épicés ou que vous préfériez des saveurs plus douces, notre menu
-                    offre quelque chose pour satisfaire toutes les papilles gustatives. Venez nous rejoindre pour une
-                    expérience culinaire authentique et chaleureuse dans une ambiance conviviale et accueillante. ¡Buen
-                    provecho!</Card.Body>
+                    <Card.Body>
+                        <p> Bienvenue dans notre restaurant mexicain, où l'authenticité et la saveur se rencontrent pour créer une
+                            expérience culinaire inoubliable. Plongez dans un voyage gustatif à travers le Mexique avec notre menu
+                            varié, mettant en vedette des plats traditionnels préparés avec amour et expertise.
+                        </p>
+                        <p>
+                            Chaque bouchée est une explosion de saveurs, que ce soit avec nos tacos à l’unité, des enchiladas
+                            généreusement garnies ou notre mole poblano exquis. Nous croyons en l'utilisation d'ingrédients frais
+                            et de qualité pour créer des plats qui vous transportent directement au cœur de la cuisine mexicaine.
+                        </p>
+                        <p>
+                            Que vous soyez un amateur de plats épicés ou que vous préfériez des saveurs plus douces, notre menu
+                            offre quelque chose pour satisfaire toutes les papilles gustatives. Venez nous rejoindre pour une
+                            expérience culinaire authentique et chaleureuse dans une ambiance conviviale et accueillante.
+                        </p>
+                        <p> 
+                            ¡Buen provecho!
+                        </p>
+                    </Card.Body>
                 </Card>
                 </Col>
             </Row>
