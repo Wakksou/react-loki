e6989f3f43d4aada96a5fc1e4e2dcf4a5d967770
Max <max94220@hotmail.fr>
2024-04-10 17:07:30 +0200

    act_18

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,28 +3,32 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Dish from "../components/Dish.jsx";
 import Button from 'react-bootstrap/Button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import dishesDatas from "../datas/dishes.json"
 
 
 const Home = () => {
-    const dish = [
-        { name: "Tacos à l'unité", img: 'https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg', price: 3, slug: 'tacos' , isNew:"false"},
-        { name: "Enchiladas", img: 'https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg', price: 12, slug: 'Enchiladas' , isNew:"true"},
-        { name: "Mole poblano", img: 'https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg', price: 15, slug: 'mole-poblano' , isNew:"true"}
-    ];
     const [showNewOnly, setShowNewOnly] = useState(false);
+    const [dishes, setDishes] = useState([]);
+
+    useEffect(() => {
+        const filteredDishes = showNewOnly
+        ? dishesDatas.filter((dish) => dish.isNew)
+        : dishesDatas;
+        setDishes(filteredDishes);
+    }, [showNewOnly]);
+
     const handleShowNewOnly = () => {
         setShowNewOnly(!showNewOnly); 
     };
-    const filteredDishes = showNewOnly ? dish.filter(item => item.isNew === "true") : dish;
     return (
         <Container>
             <Button onClick={handleShowNewOnly} variant="secondary" size="sm">
                 {showNewOnly ? "Voir tous les plats" : "Nouveautés Uniquement"}
             </Button>
             <Row>
-                {filteredDishes.map(dish => (
-                    <Col key={dish.slug}>
+                {dishes.length > 0 && dishes.map(dish => (
+                    <Col xs={4} key={dish.slug}>
                         <Dish 
                         name={dish.name}
                         img={dish.img}
