79a33e22d3d4e498a020cb0046c69b41c743e073
Max <max94220@hotmail.fr>
2024-04-10 15:50:05 +0200

    act_17

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,17 +8,22 @@ import { useState } from 'react';
 
 const Home = () => {
     const dish = [
-        { name: "Tacos à l'unité", img: 'https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg', price: 3, slug: 'tacos' , isNew:"true"},
-        { name: "Enchiladas", img: 'https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg', price: 12, slug: 'Enchiladas' , isNew:"false"},
-        { name: "Mole poblano", img: 'https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg', price: 15, slug: 'mole-poblano' , isNew:"false"}
+        { name: "Tacos à l'unité", img: 'https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg', price: 3, slug: 'tacos' , isNew:"false"},
+        { name: "Enchiladas", img: 'https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg', price: 12, slug: 'Enchiladas' , isNew:"true"},
+        { name: "Mole poblano", img: 'https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg', price: 15, slug: 'mole-poblano' , isNew:"true"}
     ];
+    const [showNewOnly, setShowNewOnly] = useState(false);
+    const handleShowNewOnly = () => {
+        setShowNewOnly(!showNewOnly); 
+    };
+    const filteredDishes = showNewOnly ? dish.filter(item => item.isNew === "true") : dish;
     return (
         <Container>
-            <Button variant="secondary" size="sm">
-                Nouveautés Uniquement
+            <Button onClick={handleShowNewOnly} variant="secondary" size="sm">
+                {showNewOnly ? "Voir tous les plats" : "Nouveautés Uniquement"}
             </Button>
             <Row>
-                {dish.map(dish => (
+                {filteredDishes.map(dish => (
                     <Col key={dish.slug}>
                         <Dish 
                         name={dish.name}
