4f23ba86d46ec7c2980a3ba50b4eb68b28ee54c1
Max <max94220@hotmail.fr>
2024-04-12 11:23:41 +0200

    act_23

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,6 +6,8 @@ import { useContext, useEffect, useState } from "react";
 import useTotalQuantity from "../utils/hooks/useTotalQuantity"; 
 import useTotalPrice from "../utils/hooks/useTotalPrice"; 
 import "../assets/styles/cart.css";
+import React from 'react';
+import { Helmet } from 'react-helmet';
 
 const Cart = () => {
     const { cart } = useContext(CartContext);
@@ -13,35 +15,40 @@ const Cart = () => {
     const totalPrice = useTotalPrice();  
 
     return (
-        <Container>
-            <Row>
-                <h1>Panier</h1>
-            </Row>
-            <Row>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Nom</th>
-                            <th>Prix</th>
-                            <th>Quantités</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {cart && cart.map((item, index) => (
-                            <tr key={index}>
-                                <td>{item.name}</td>
-                                <td>{item.price}€</td>
-                                <td>{item.quantity}</td>
+        <>
+            <Helmet>
+                <title>Page non trouvée - Mon super site</title>
+            </Helmet>
+            <Container>
+                <Row>
+                    <h1>Panier</h1>
+                </Row>
+                <Row>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Nom</th>
+                                <th>Prix</th>
+                                <th>Quantités</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </Table>
-                <div class="total">
-                    <p>Total : {totalQuantity} articles</p>
-                    <p>Prix : {totalPrice} euros</p>
-                </div>
-            </Row>
-        </Container>
+                        </thead>
+                        <tbody>
+                            {cart && cart.map((item, index) => (
+                                <tr key={index}>
+                                    <td>{item.name}</td>
+                                    <td>{item.price}€</td>
+                                    <td>{item.quantity}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                    <div class="total">
+                        <p>Total : {totalQuantity} articles</p>
+                        <p>Prix : {totalPrice} euros</p>
+                    </div>
+                </Row>
+            </Container>
+        </>
     );
 };
 
